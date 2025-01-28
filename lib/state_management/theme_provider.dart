798f3f7fc22d706222352aa7210c2af82a92e36a
Tsabitah59 <tsabitahsafa06@gmail.com>
2025-01-28 08:17:44 +0700

    adding timestamp service

--- lib/state_management/theme_provider.dart
+++ lib/state_management/theme_provider.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier{
   bool _isDarkTheme = false;
 
   ThemeProvider() {
-    // _loadTheme();
+    _loadTheme();
   }
 
   bool get isDarkTheme => _isDarkTheme;
 
-  void toggleTheme() {
-    
+  void toggleTheme(bool isDark) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    _isDarkTheme = isDark;
+    await prefs.setBool('isDarkTheme', isDark);
+    notifyListeners();
+  }
+
+  void _loadTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _isDarkTheme = prefs.getBool('isDarkTheme') ?? false;
+    notifyListeners();
   }
 }
