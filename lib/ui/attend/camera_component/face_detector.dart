af3172b2d390aa4527c13297a634652248634b09
Tsabitah59 <tsabitahsafa06@gmail.com>
2025-02-05 14:01:07 +0700

    create error_handler

--- lib/ui/attend/camera_component/face_detector.dart
+++ lib/ui/attend/camera_component/face_detector.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+import 'dart:ui';
 import 'package:attendence_app/utils/google_ml_kit.dart';
 import 'package:camera/camera.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
@@ -13,5 +15,29 @@ class FaceDetectorComponent {
   Future<void> detectFace(XFile image) async {
     final inputImage = InputImage.fromFilePath(image.path);
     final faces = await faceDetector.processImage(inputImage);
+    
+      for (Face face in faces) {
+      final Rect boundingBox = face.boundingBox;
+      // untuk menghandle posisi wajah secara vertikal
+      final double? verticalPosition = face.headEulerAngleY;
+      // untuk menghandle posisi wajah secara horizontal
+      final double? horizontalPosition = face.headEulerAngleZ;
+      // perkondisian apabila face landmark sudah aktif, ditandai oleh (mulut, mata, pipi, hidung, dan telinga)
+      final FaceLandmark? leftEar = face.landmarks[FaceLandmarkType.leftEar];
+      if (leftEar != null) {
+        final Point<int> leftEarPosition = leftEar.position;
+        print("Left Ear Position: $leftEarPosition");
+      }
+      // perkondisian apabila wajahnya terdeteksi (ditandai dengan bibir tersenyum)
+      if (face.smilingProbability != null) {
+        final double? smileProbability = face.smilingProbability;
+        print("Smile Probability: $smileProbability");
+      }
+      // perkondisian apabila fitur tracking wajah sudah aktif
+      if (face.trackingId != null) {
+        final int? trackingId = face.trackingId;
+        print("Tracking ID: $trackingId");
+      }
+    }
   }
 }
